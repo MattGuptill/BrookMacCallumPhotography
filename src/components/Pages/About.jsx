f6bfca04504a68145a39b201214d308c69203474
Matthew Guptill <matthewdguptill7@gmail.com>
2024-01-31 19:05:34 -0600

    started about page

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import matt from '/src/assets/MatthewGuptill.jpg'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <article className='h-fit flex flex-col px-11/12 py-10 '>
+      <img src={matt} alt="an example photo of matt in front of forest scenery" />
+    </article>
   )
 }
